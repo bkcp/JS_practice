2d79bc0378c9b1fe2a239ee76c3bf9aeda7887bb
bkcp <bkcp83@gmail.com>
2022-03-03 06:38:52 -0800

    practice

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ let synth = "NerdSeq";
 if (synth === "NerdSeq") {
   console.log(`Damn thats a nice ${synth}`);
 }
+
+let notes = ["C", "D", "E", "F", "G"];
+
+notes[5] = "fuck";
+console.log(notes[5]);
+console.log(notes.length);
