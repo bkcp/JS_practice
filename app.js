07c079dae0e72818274b3e90e6a03f7be6c48049
bkcp <bkcp83@gmail.com>
2022-03-14 16:15:45 -0700

    guessing game

--- app.js
+++ app.js
@@ -131,27 +131,27 @@ console.log("congrats");
 // }
 // console.log("Ok you win");
 
-let maximum = parseInt(prompt("enter max number"));
-while (!maximum) {
-  maximum = parseInt(prompt("a NUMBER dumbass"));
-}
+// let maximum = parseInt(prompt("enter max number"));
+// while (!maximum) {
+//   maximum = parseInt(prompt("a NUMBER dumbass"));
+// }
 
-const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
+// const targetNum = Math.floor(Math.random() * maximum) + 1;
+// console.log(targetNum);
 
-let guess = parseInt(prompt("enter a guess"));
-let attempts = 1;
-while (parseInt(guess) !== targetNum) {
-  if (guess === "q") break;
-  attempts++;
-  if (guess > targetNum) {
-    guess = prompt("too high");
-  } else {
-    guess = prompt("too low");
-  }
-}
-if (guess === "q") {
-  console.log("ok fine quit.");
-} else {
-  console.log(`Nice! it took ${attempts} attempts`);
-}
+// let guess = parseInt(prompt("enter a guess"));
+// let attempts = 1;
+// while (parseInt(guess) !== targetNum) {
+//   if (guess === "q") break;
+//   attempts++;
+//   if (guess > targetNum) {
+//     guess = prompt("too high");
+//   } else {
+//     guess = prompt("too low");
+//   }
+// }
+// if (guess === "q") {
+//   console.log("ok fine quit.");
+// } else {
+//   console.log(`Nice! it took ${attempts} attempts`);
+// }
