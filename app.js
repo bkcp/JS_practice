20926ada1231af9a0e8b10f5e019b1b7878595df
bkcp <bkcp83@gmail.com>
2022-03-26 07:55:50 -0700

    Array functions. For Each

--- app.js
+++ app.js
@@ -438,3 +438,15 @@ function yell(msg) {
     console.log("please use a string");
   }
 }
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// function print(element) {
+//   console.log(element);
+// }
+// numbers.forEach(print);
+
+numbers.forEach(function (el) {
+  if (el % 2 === 0) {
+    console.log(el);
+  }
+});
