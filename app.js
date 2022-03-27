9205ce67263079d02d1f1311f4b582fc31ebc826
bkcp <bkcp83@gmail.com>
2022-03-26 17:34:20 -0700

    more for each array function practice

--- app.js
+++ app.js
@@ -438,6 +438,8 @@ function yell(msg) {
     console.log("please use a string");
   }
 }
+
+//ARRAY functions
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
 // function print(element) {
@@ -449,4 +451,27 @@ numbers.forEach(function (el) {
   if (el % 2 === 0) {
     console.log(el);
   }
+});
+
+const movies = [
+  {
+    title: "Amadeus",
+    score: 99,
+  },
+  {
+    title: "Stand by me",
+    score: 59,
+  },
+  {
+    title: "Parasite",
+    score: 95,
+  },
+  {
+    title: "Alien",
+    score: 100,
+  },
+];
+
+movies.forEach(function (el) {
+  console.log(`${el.title} - ${el.score} /100`);
 });
